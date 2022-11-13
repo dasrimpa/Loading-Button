89fb2785af7917aa5d782f2a815167fdb82f0275
Rimpa Das <rimpa@itobuz.com>
2022-11-13 17:44:55 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+     <Button color='purple' >Submit</Button>
+    </div>
+  );
+}
+
+export default App;
